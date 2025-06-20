0978289fcaab0d54c3cd662a749f8a5f604ff0d1
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-06-20 16:02:27 +0600

    ui modification - limited stock

--- src/HRLayout/HRHome/HRHomeSubComponents/LimitedStockItems.jsx
+++ src/HRLayout/HRHome/HRHomeSubComponents/LimitedStockItems.jsx
@@ -2,11 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPrivate from "../../../Hooks/useAxiosPrivate";
 import { useContext } from "react";
 import { UserMainContext } from "../../../Context/UserContext";
-import { FaPenToSquare } from "react-icons/fa6";
+import { FaPenToSquare, FaRegCircleCheck } from "react-icons/fa6";
 import { FaTrashAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router";
 import { DNA } from "react-loader-spinner";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const LimitedStockItems = () => {
   const AxiosSecure = useAxiosPrivate();
@@ -62,7 +63,7 @@ const LimitedStockItems = () => {
     <div className="w-full">
       <div className="w-full flex flex-col p-2 items-center mt-16">
         <h2 className="lg:text-5xl sm:text-3xl text-2xl font-semibold">
-          Limited Stock Assets
+          Limited Stock
         </h2>
         <p className="sm:text-lg font-medium text-sky-600 mt-2 text-center">
           Check assets that are currently running low in stock.
@@ -74,11 +75,11 @@ const LimitedStockItems = () => {
           <DNA></DNA>
         </div>
       ) : limited.length > 0 ? (
-        <div className="w-full mx-auto px-2 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-center  gap-3 md:mt-10 sm:mt-8 mt-6">
+        <div className="lg:w-11/12 w-full mx-auto px-5 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-center gap-5 md:mt-10 sm:mt-8 mt-6">
           {limited?.map((asset) => (
             <div
               key={asset._id}
-              className="max-w-xs mx-auto w-full h-full p-4 border-2 flex flex-col justify-between gap-1 rounded-md border-orange-600"
+              className="max-w-xs mx-auto w-full h-full p-4 border-2 flex flex-col justify-between gap-1 border-amber-100 bg-gradient-to-t from-amber-100 via-white to-white shadow-md transition-all hover:from-amber-200 hover:shadow-xl duration-300"
             >
               <h2 className="md:text-3xl text-2xl font-semibold text-start">
                 {asset.name}
@@ -93,16 +94,28 @@ const LimitedStockItems = () => {
                       {asset.quantity}
                     </p>
                   </div>
-                  <div className="flex flex-col">
+                  <div className="flex flex-col items-center gap-1">
                     <span className="text-sm font-medium text-gray-500">
-                      Type:
+                      Returnable:
                     </span>
-                    <p className="md:text-lg text-base font-bold text-lime-800 text-nowrap">
-                      {asset.type}
-                    </p>
+                    {asset.type === "returnable" ? (
+                      <FaRegCircleCheck
+                        data-tooltip-id="my-tooltip"
+                        data-tooltip-content={"Yes"}
+                        data-tooltip-place="left"
+                        className="text-3xl text-orange-500"
+                      ></FaRegCircleCheck>
+                    ) : (
+                      <AiOutlineCloseCircle
+                        data-tooltip-id="my-tooltip"
+                        data-tooltip-content={"No"}
+                        data-tooltip-place="left"
+                        className="text-3xl text-lime-500"
+                      ></AiOutlineCloseCircle>
+                    )}
                   </div>
                 </div>
-                <p className="md:text-lg text-base font-medium text-black flex gap-1 mt-3">
+                <p className="md:text-lg text-base font-medium text-black flex gap-1 mt-3 flex-wrap">
                   <span className="font-semibold text-gray-500">
                     Entry Date:
                   </span>
